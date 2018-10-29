db9978b98e48969a10f1f7f6e056e401422fc624
NaiShengYu <2495024529@qq.com>
2018-10-29 13:52:27 +0800

    Initial Commit

---
+++ WKWebCallJSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebCallJSDemo
+//
+//  Created by Nasheng Yu on 2018/10/29.
+//  Copyright © 2018年 Nasheng Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
